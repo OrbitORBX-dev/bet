95f3fd4279bff700b47218de99793a3c81f6fc17
Orion Group <btgod443@gmail.com>
2025-12-30 22:54:24 +0000

    Update stellar.js

--- server/stellar.js
+++ server/stellar.js
@@ -1,1 +1,9 @@
+import StellarSdk from "stellar-sdk";
 
+export const server = new StellarSdk.Server(
+  "https://horizon.stellar.org"
+);
+
+export const ORNX = new StellarSdk.Asset(
+  "ORNX",
+  "ISSUER_PUBLIC_KEY"
