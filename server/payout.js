c4796f671fd164a97fd90550bc4470ccbce809a2
Orion Group <btgod443@gmail.com>
2025-12-30 22:59:40 +0000

    Update payout.js

--- server/payout.js
+++ server/payout.js
@@ -1,1 +1,22 @@
+import StellarSdk from "stellar-sdk";
+import { ORBX, server } from "./stellar.js";
 
+export async function payoutWinner(destination, amount, secret) {
+  const source = StellarSdk.Keypair.fromSecret(secret);
+  const account = await server.loadAccount(source.publicKey());
+
+  const tx = new StellarSdk.TransactionBuilder(account, {
+    fee: StellarSdk.BASE_FEE,
+    networkPassphrase: StellarSdk.Networks.PUBLIC
+  })
+    .addOperation(StellarSdk.Operation.payment({
+      destination,
+      asset: ORNX,
+      amount: amount.toString()
+    }))
+    .setTimeout(30)
+    .build();
+
+  tx.sign(source);
+  await server.submitTransaction(tx);
+}
