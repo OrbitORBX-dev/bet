390f63c8d49a9eabb97d88067108139168b9b94d
Orion Group <btgod443@gmail.com>
2025-12-30 22:52:11 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "./watcher.js";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("Bet backend running");
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on port 3000");
+});
