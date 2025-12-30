d2b9ebfc620354bbcc3a9f9b607448a90d370bb6
Orion Group <btgod443@gmail.com>
2025-12-30 22:55:30 +0000

    Update watcher.js

--- server/watcher.js
+++ server/watcher.js
@@ -1,1 +1,17 @@
+import { server, ORBX } from "./stellar.js";
 
+const HOUSE_ESCROW = "HOUSE_PUBLIC_KEY";
+
+server.payments()
+  .forAccount(HOUSE_ESCROW)
+  .cursor("now")
+  .stream({
+    onmessage: payment => {
+      if (
+        payment.asset_code === "ORNX" &&
+        payment.from !== HOUSE_ESCROW
+      ) {
+        console.log("New bet received:", payment.amount);
+      }
+    }
+  });
